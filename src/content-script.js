962ae5ac46a3fc0ef008ac4570bd5d9ffca6caf4
Boris Bera <bera.boris@gmail.com>
2020-02-26 19:14:15 -0500

    Add basic extension that adds a border
    It's dirty but it has a build and the likes so it's a good start

---
+++ src/content-script.js
@@ -0,0 +1,3 @@
+console.log('hello world from script')
+document.documentElement.style.border = 'solid 3px red'
+document.body.style.border = 'solid 3px pink'
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+function isProd () {
+  return process.env.NODE_ENV === 'production'
+}
+
+module.exports = {
+  mode: isProd() ? 'production' : 'development',
+
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    'content-script': './content-script'
+  },
+
+  plugins: [
+    new CopyPlugin([
+      'manifest.json'
+    ])
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+
+  devtool: 'source-map'
+}
