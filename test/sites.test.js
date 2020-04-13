afc205929c2c197b9c5d7d2440d60acf61fc8c4c
Boris Bera <bera.boris@gmail.com>
2020-04-13 15:40:28 -0400

    Use Sites with capital S in tests

--- test/sites.test.js
+++ test/sites.test.js
@@ -1,35 +1,35 @@
 import './test-helper'
 import { expect } from 'chai'
-import * as sites from '../src/sites'
+import * as Sites from '../src/sites'
 
 describe('sites.js', () => {
   it('should start with an empty db', () => {
-    const db = sites.createDb()
+    const db = Sites.createDb()
 
-    const res = sites.getAll(db)
+    const res = Sites.getAll(db)
 
     expect(res).to.deep.eq([])
   })
 
   describe('add()', () => {
     it('should add to the sites', () => {
-      const db = sites.createDb()
+      const db = Sites.createDb()
 
-      sites.add(db, { phony: 1 })
-      const res = sites.getAll(db)
+      Sites.add(db, { phony: 1 })
+      const res = Sites.getAll(db)
 
       expect(res).to.have.lengthOf(1)
       expect(res[0]).to.have.property('phony', 1)
     })
 
     it('should not generate duplicate ids', () => {
-      const db = sites.createDb()
+      const db = Sites.createDb()
 
-      sites.add(db, {})
-      sites.add(db, {})
-      sites.add(db, {})
-      sites.add(db, {})
-      const res = sites.getAll(db)
+      Sites.add(db, {})
+      Sites.add(db, {})
+      Sites.add(db, {})
+      Sites.add(db, {})
+      const res = Sites.getAll(db)
 
       expect(res).to.have.lengthOf(4)
       const ids = Array.from(new Set(res.map(site => site.id)))
@@ -39,15 +39,15 @@ describe('sites.js', () => {
 
   describe('addAll()', () => {
     it('should add to the sites', () => {
-      const db = sites.createDb()
+      const db = Sites.createDb()
 
-      sites.addAll(db, [
+      Sites.addAll(db, [
         { phony: 'a' },
         { phony: 'b' },
         { phony: 'c' },
         { phony: 'd' }
       ])
-      const res = sites.getAll(db)
+      const res = Sites.getAll(db)
 
       const phonies = res.map(site => site.phony)
       phonies.sort()
@@ -55,10 +55,10 @@ describe('sites.js', () => {
     })
 
     it('should not generate duplicate ids', () => {
-      const db = sites.createDb()
+      const db = Sites.createDb()
 
-      sites.addAll(db, [{}, {}, {}, {}])
-      const res = sites.getAll(db)
+      Sites.addAll(db, [{}, {}, {}, {}])
+      const res = Sites.getAll(db)
 
       expect(res).to.have.lengthOf(4)
       const ids = Array.from(new Set(res.map(site => site.id)))
@@ -68,41 +68,41 @@ describe('sites.js', () => {
 
   describe('get()', () => {
     it('should get by id', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{ phony: 42 }])
-      const id = sites.getAll(db)[0].id
+      const db = Sites.createDb()
+      Sites.addAll(db, [{ phony: 42 }])
+      const id = Sites.getAll(db)[0].id
 
-      const res = sites.get(db, id)
+      const res = Sites.get(db, id)
 
       expect(res).to.have.property('phony', 42)
     })
 
     it('should not return id', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{ phony: 42 }])
-      const id = sites.getAll(db)[0].id
+      const db = Sites.createDb()
+      Sites.addAll(db, [{ phony: 42 }])
+      const id = Sites.getAll(db)[0].id
 
-      const res = sites.get(db, id)
+      const res = Sites.get(db, id)
 
       expect(res).to.not.have.property('id')
     })
 
     it('should return null if nothing is found', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{ phony: 42 }])
-      const id = sites.getAll(db)[0].id
+      const db = Sites.createDb()
+      Sites.addAll(db, [{ phony: 42 }])
+      const id = Sites.getAll(db)[0].id
 
-      const res = sites.get(db, id + 1)
+      const res = Sites.get(db, id + 1)
 
       expect(res).to.be.null()
     })
 
     it('should not coerce between strings and numbers', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{ phony: 42 }])
-      const id = sites.getAll(db)[0].id
+      const db = Sites.createDb()
+      Sites.addAll(db, [{ phony: 42 }])
+      const id = Sites.getAll(db)[0].id
 
-      const res = sites.get(db, String(id))
+      const res = Sites.get(db, String(id))
 
       expect(res).to.be.null()
     })
@@ -110,34 +110,34 @@ describe('sites.js', () => {
 
   describe('remove()', () => {
     it('should remove by id', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{ phony: 42 }])
-      const id = sites.getAll(db)[0].id
+      const db = Sites.createDb()
+      Sites.addAll(db, [{ phony: 42 }])
+      const id = Sites.getAll(db)[0].id
 
-      sites.remove(db, id)
-      const res = sites.getAll(db)
+      Sites.remove(db, id)
+      const res = Sites.getAll(db)
 
       expect(res).to.be.deep.eq([])
     })
 
     it('should not remove when id does not match', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{ phony: 42 }])
-      const id = sites.getAll(db)[0].id
+      const db = Sites.createDb()
+      Sites.addAll(db, [{ phony: 42 }])
+      const id = Sites.getAll(db)[0].id
 
-      sites.remove(db, id + 1)
-      const res = sites.getAll(db)
+      Sites.remove(db, id + 1)
+      const res = Sites.getAll(db)
 
       expect(res).to.have.lengthOf(1)
     })
 
     it('should not coerce between string and int ids', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{ phony: 42 }])
-      const id = sites.getAll(db)[0].id
+      const db = Sites.createDb()
+      Sites.addAll(db, [{ phony: 42 }])
+      const id = Sites.getAll(db)[0].id
 
-      sites.remove(db, String(id))
-      const res = sites.getAll(db)
+      Sites.remove(db, String(id))
+      const res = Sites.getAll(db)
 
       expect(res).to.have.lengthOf(1)
     })
@@ -145,56 +145,56 @@ describe('sites.js', () => {
 
   describe('update()', () => {
     it('should replace the value when id matches', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{}, {}])
-      const [first, second] = sites.getAll(db)
+      const db = Sites.createDb()
+      Sites.addAll(db, [{}, {}])
+      const [first, second] = Sites.getAll(db)
 
-      sites.update(db, first.id, { changed: true })
+      Sites.update(db, first.id, { changed: true })
 
-      const changed = sites.get(db, first.id)
+      const changed = Sites.get(db, first.id)
       expect(changed).to.have.property('changed', true)
-      const other = sites.get(db, second.id)
+      const other = Sites.get(db, second.id)
       expect(other).not.to.have.property('changed')
     })
 
     it('should do nothing when id does not exist', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{}, {}])
-      const [first, second] = sites.getAll(db)
+      const db = Sites.createDb()
+      Sites.addAll(db, [{}, {}])
+      const [first, second] = Sites.getAll(db)
 
-      sites.update(db, second.id + 20, { changed: true })
+      Sites.update(db, second.id + 20, { changed: true })
 
-      const firstRes = sites.get(db, first.id)
+      const firstRes = Sites.get(db, first.id)
       expect(firstRes).to.not.have.property('changed')
-      const secondRes = sites.get(db, second.id)
+      const secondRes = Sites.get(db, second.id)
       expect(secondRes).to.not.have.property('changed')
     })
 
     it('should not coerce between string and int ids', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [{}, {}])
-      const [first, second] = sites.getAll(db)
+      const db = Sites.createDb()
+      Sites.addAll(db, [{}, {}])
+      const [first, second] = Sites.getAll(db)
 
-      sites.update(db, String(first.id), { changed: true })
+      Sites.update(db, String(first.id), { changed: true })
 
-      const firstRes = sites.get(db, first.id)
+      const firstRes = Sites.get(db, first.id)
       expect(firstRes).to.not.have.property('changed')
-      const secondRes = sites.get(db, second.id)
+      const secondRes = Sites.get(db, second.id)
       expect(secondRes).to.not.have.property('changed')
     })
   })
 
   describe('findMatching()', () => {
     it('should return sites where pattern matches url', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [
+      const db = Sites.createDb()
+      Sites.addAll(db, [
         { pattern: 'fo' },
         { pattern: 'foo' },
         { pattern: 'bar' },
         { pattern: 'baz' }
       ])
 
-      const matches = sites.findMatching(db, 'https://foobar.com')
+      const matches = Sites.findMatching(db, 'https://foobar.com')
 
       const patterns = matches.map(site => site.pattern).sort()
       expect(patterns).to.deep.eq([
@@ -205,24 +205,24 @@ describe('sites.js', () => {
     })
 
     it('should return empty array with no matches', () => {
-      const db = sites.createDb()
-      sites.addAll(db, [
+      const db = Sites.createDb()
+      Sites.addAll(db, [
         { pattern: 'fo' },
         { pattern: 'foo' },
         { pattern: 'bar' },
         { pattern: 'baz' }
       ])
 
-      const matches = sites.findMatching(db, 'https://something-else.com')
+      const matches = Sites.findMatching(db, 'https://something-else.com')
 
       const patterns = matches.map(site => site.pattern).sort()
       expect(patterns).to.deep.eq([])
     })
 
     it('should return empty array with no sites', () => {
-      const db = sites.createDb()
+      const db = Sites.createDb()
 
-      const matches = sites.findMatching(db, 'https://something-else.com')
+      const matches = Sites.findMatching(db, 'https://something-else.com')
 
       const patterns = matches.map(site => site.pattern).sort()
       expect(patterns).to.deep.eq([])
