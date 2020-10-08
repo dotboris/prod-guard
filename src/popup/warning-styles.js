05e90d8c1338c71de271655a8ad5e45ca4e5dda9
Annie-Claude Morin <acmorin@pmctire.com>
2020-10-08 18:45:14 -0400

    Add definition for warning styles to specify if they have text

--- src/popup/warning-styles.js
+++ src/popup/warning-styles.js
@@ -1,5 +1,14 @@
-export const names = {
-  topBanner: 'Top Banner',
-  bottomBanner: 'Bottom Banner',
-  border: 'Border'
+export const definitions = {
+  topBanner: {
+    label: 'Top Banner',
+    hasText: true
+  },
+  bottomBanner: {
+    label: 'Bottom Banner',
+    hasText: true
+  },
+  border: {
+    label: 'Border',
+    hasText: false
+  }
 }
