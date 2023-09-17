494f45a9c64778d36a351f63d242902a40daea52
Boris Bera <beraboris@gmail.com>
2023-09-17 13:04:25 -0400

    Add header to export section in settings

--- src/popup/settings/settings-page.tsx
+++ src/popup/settings/settings-page.tsx
@@ -4,6 +4,7 @@ import ExportBox from './export'
 export default function SettingsPage(): JSX.Element {
   return (
     <Layout title='Settings'>
+      <h2>Export All Data</h2>
       <ExportBox />
     </Layout>
   )
