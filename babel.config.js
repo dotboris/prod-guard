e67413dad0b46da1b3dd8c6612ba6aaa203b8aa1
Boris Bera <beraboris@gmail.com>
2023-09-17 11:34:31 -0400

    Add UI for export box

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = {
   presets: [
     '@babel/preset-typescript',
     '@babel/preset-env',
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    [
+      '@babel/preset-react',
+      { runtime: 'automatic', importSource: '@emotion/react' },
+    ],
   ],
+  plugins: ['@emotion'],
 }
--- src/popup/settings/export.tsx
+++ src/popup/settings/export.tsx
@@ -1,8 +1,77 @@
-import { type JSX } from 'react'
+import { css } from '@emotion/react'
+import { useState, type JSX, useEffect } from 'react'
+import { useAsyncFn } from 'react-use'
+
+const styles = {
+  root: css({
+    display: 'grid',
+    gap: '0.5rem',
+  }),
+  textBox: css({
+    height: '8rem',
+  }),
+}
 
 export default function ExportBox(): JSX.Element {
   const data = { foo: 'bar' } // TODO: real data
   const formattedData = JSON.stringify(data, null, 2)
 
-  return <textarea readOnly>{formattedData}</textarea>
+  return (
+    <div css={styles.root}>
+      <textarea css={styles.textBox} readOnly value={formattedData} />
+      <CopyToClipboardButton text={formattedData} />
+    </div>
+  )
+}
+
+function CopyToClipboardButton({ text }: { text: string }): JSX.Element {
+  const [copiedRecently, setCopiedRecently] = useExpiringState(false, 2000)
+  const [state, doCopy] = useAsyncFn(async () => {
+    try {
+      await navigator.clipboard.writeText(text)
+    } finally {
+      setCopiedRecently(true)
+    }
+  }, [text, setCopiedRecently])
+
+  let buttonText = 'Copy to Clipboard'
+  if (copiedRecently) {
+    if (state.error == null) {
+      buttonText = 'Copied!'
+    } else {
+      buttonText = 'Failed to Copy'
+    }
+  }
+
+  return (
+    <>
+      <button
+        type='button'
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onClick={state.loading ? undefined : doCopy}
+        disabled={state.loading}
+      >
+        {buttonText}
+      </button>
+      {state.error != null ? <p>{state.error.message}</p> : null}
+    </>
+  )
+}
+
+function useExpiringState<T>(
+  defaultValue: T,
+  timeout: number,
+): [T, (value: T) => void] {
+  const [value, setValue] = useState(defaultValue)
+  useEffect(() => {
+    if (value !== defaultValue) {
+      const handle = setTimeout(() => {
+        setValue(defaultValue)
+      }, timeout)
+      return () => {
+        clearTimeout(handle)
+      }
+    }
+  }, [defaultValue, timeout, value, setValue])
+  return [value, setValue]
 }
