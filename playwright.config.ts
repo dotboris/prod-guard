6b8ef76a6348310064c04c64afb622b167a943b2
Boris Bera <beraboris@gmail.com>
2024-03-24 18:50:04 -0400

    Bump treshold for screnshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,4 +5,12 @@ export default defineConfig({
   use: {
     trace: 'on',
   },
+  expect: {
+    toHaveScreenshot: {
+      // There are slight differences in font rendering between the CI
+      // environment and developer desktop environments. This makes the diff a
+      // bit more tolerant.
+      threshold: 0.4,
+    },
+  },
 })
