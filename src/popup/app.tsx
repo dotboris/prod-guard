9c48da78e635b100a5d0c582d91359c632d5e465
Boris Bera <beraboris@gmail.com>
2024-12-14 17:47:51 -0500

    chore(popup): remove useless popup.html redirect
    I think that we used to have this around when we used the navigation API
    but now that we use a hash router things just work.

--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -1,14 +1,12 @@
+import { Routes, Route } from "react-router";
 import WarningsListPage from "./warnings/list-page";
 import NewWarningPage from "./warnings/new-page";
 import EditWarningPage from "./warnings/edit-page";
-import { Routes, Route, Navigate } from "react-router";
 import SettingsPage from "./settings/settings-page";
 
 export default function App() {
   return (
     <Routes>
-      {/* The popup starts at /popup.html. We normalize that to / */}
-      <Route path="/popup.html" element={<Navigate to="/" replace />} />
       <Route path="/" element={<WarningsListPage />} />
       <Route path="/new" element={<NewWarningPage />} />
       <Route path="/edit/:id" element={<EditWarningPage />} />
