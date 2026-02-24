df72ecde836085a4ab64cfcd48746296796cc5fd
Boris Bera <beraboris@gmail.com>
2026-02-23 19:46:33 -0500

    chore: add story for settings page

---
+++ src/popup/settings/settings-page.stories.tsx
@@ -0,0 +1,77 @@
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
+
+import SettingsPage from "./settings-page";
+import { mocked } from "storybook/test";
+import browser from "webextension-polyfill";
+import { AllData } from "../../schema";
+
+const meta = {
+  component: SettingsPage,
+  beforeEach: () => {
+    mocked(browser, true).storage.sync.get.mockResolvedValue({
+      dataVersion: 4,
+      warnings: [
+        {
+          id: "bogus1",
+          warningStyle: "topBanner",
+          enabled: true,
+          pattern: "example\\.com",
+          text: "Example Warning",
+          backgroundColor: "FF8800",
+          textColor: "FFFFFF",
+        },
+        {
+          id: "bogus2",
+          warningStyle: "topBanner",
+          enabled: false,
+          pattern: "example\\.com",
+          text: "Example Warning",
+          backgroundColor: "FF8800",
+          textColor: "FFFFFF",
+        },
+        {
+          id: "bogus3",
+          warningStyle: "bottomBanner",
+          enabled: true,
+          pattern: "example\\.com",
+          text: "Example Warning",
+          backgroundColor: "FF8800",
+          textColor: "FFFFFF",
+        },
+        {
+          id: "bogus4",
+          warningStyle: "bottomBanner",
+          enabled: false,
+          pattern: "example\\.com",
+          text: "Example Warning",
+          backgroundColor: "FF8800",
+          textColor: "FFFFFF",
+        },
+        {
+          id: "bogus5",
+          warningStyle: "border",
+          enabled: true,
+          pattern: "example\\.com",
+          borderColor: "FF8800",
+        },
+        {
+          id: "bogus6",
+          warningStyle: "border",
+          enabled: false,
+          pattern: "example\\.com",
+          borderColor: "FF8800",
+        },
+      ],
+    } satisfies AllData);
+    mocked(browser, true).storage.sync.set.mockImplementation((data) => {
+      alert("import: " + JSON.stringify(data, null, 2));
+      return Promise.resolve();
+    });
+  },
+} satisfies Meta<typeof SettingsPage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
